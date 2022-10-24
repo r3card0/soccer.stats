2ec21ef6da1bed4d3a651c6502b4bf51c9adee14
r3card0 <ricardo.rdz@pm.me>
2022-10-23 20:23:22 -0500

    insert stadiums data

---
+++ soccer/manipulation/insert_stadiums.py
@@ -0,0 +1,46 @@
+from _read_files import get_stadiums
+import mysql.connector as conn
+from config import config
+import datetime
+from insert_statements import insert_stadiums
+
+# connection
+def connectdb():
+    params = config()
+    connection = conn.connect(**params)
+    
+    return connection
+
+connection = connectdb()
+cur = connection.cursor()
+df = get_stadiums()
+insert_ = insert_stadiums()
+insert_date = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
+
+# df['values'] = df[['value1', 'value2', 'value3']].values.tolist()
+def by_row():
+    df['insert_date'] = insert_date
+    df['data'] = df[['stadium_name','city_id','insert_date']].values.tolist()
+    rows = list(df['data'])
+    return rows
+
+def insert_data():
+    data_list = []
+    lists = by_row()
+
+    for data in lists:
+        data_list.append(tuple(data)) 
+
+    #return data_list
+    cur.executemany(insert_,data_list)
+    connection.commit()
+    connection.close()
+
+
+def run():
+    insert_data()
+
+
+if __name__ == "__main__":
+    run()
