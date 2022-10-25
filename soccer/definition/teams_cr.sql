995c697a4840f96cabe7565ad8cf9c795bdbbe6f
r3card0 <ricardo.rdz@pm.me>
2022-10-25 18:09:05 -0500

    create table teams

---
+++ soccer/definition/teams_cr.sql
@@ -0,0 +1,5 @@
+CREATE TABLE IF NOT EXISTS teams(team_id INTEGER UNSIGNED PRIMARY KEY AUTO_INCREMENT
+, team_name VARCHAR(50) UNIQUE
+, country_id INTEGER
+, stadium_id INTEGER
+, insert_date DATETIME)
