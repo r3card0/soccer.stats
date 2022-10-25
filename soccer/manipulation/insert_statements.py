1b2d3a4a9d5ffe8c42624be052de0aad656e3c63
r3card0 <ricardo.rdz@pm.me>
2022-10-25 18:12:04 -0500

    added insert teams

--- soccer/manipulation/insert_statements.py
+++ soccer/manipulation/insert_statements.py
@@ -2,3 +2,7 @@ def insert_stadiums():
     insert_ = "INSERT INTO soccer.stadiums(stadium_name,city_id,insert_date) VALUES(%s,%s,%s)"
     return insert_
 
+def insert_teams():
+    insert_ = "INSERT INTO soccer.teams(team_name,stadium_id,country_id,insert_date) VALUES(%s,%s,%s,%s)"
+    return insert_
+
