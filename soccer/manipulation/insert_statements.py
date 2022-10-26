87e59de998be3e79c5d05e9f455a6c93ef1fc4f3
r3card0 <ricardo.rdz@pm.me>
2022-10-25 21:00:48 -0500

    added seasons

--- soccer/manipulation/insert_statements.py
+++ soccer/manipulation/insert_statements.py
@@ -6,3 +6,12 @@ def insert_teams():
     insert_ = "INSERT INTO soccer.teams(team_name,stadium_id,country_id,insert_date) VALUES(%s,%s,%s,%s)"
     return insert_
 
+def insert_leagues():
+    insert_ = "INSERT INTO soccer.leagues(league_name,league_level,country_id,insert_date) VALUES(%s,%s,%s,%s)"
+    return insert_
+
+
+def insert_seasons():
+    insert_ = "INSERT INTO soccer.seasons(season_name,start_date,end_date,league_id,insert_date) VALUES(%s,%s,%s,%s,%s)"
+    return insert_
+
